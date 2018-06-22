12ecc56ed5b3756ed2ced8786aec97cd099f3eb1
Rahul Krishna <krishrahul98@gmail.com>
2018-06-23 04:09:23 +0530

    Introdduced Lambda Function and ButterKnife

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -3,39 +3,48 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.numbers)
+    TextView numbers;
+    @BindView(R.id.family)
+    TextView family;
+    @BindView(R.id.colors)
+    TextView colors;
+    @BindView(R.id.phrases)
+    TextView phrases;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Set the content of the activity to use the activity_main.xml layout file
+
         setContentView(R.layout.activity_main);
-    }
-
-    // For Numbers Activity
-    public void openNumbersActivity(View view) {
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
-    }
-
-    //For Family Activity
-    public void openFamilyActivity(View view) {
-        Intent intent = new Intent(this, FamilyActivity.class);
-        startActivity(intent);
-    }
-
-    //For Colors Activity
-    public void openColorsActivity(View view) {
-        Intent intent = new Intent(this, ColorsActivity.class);
-        startActivity(intent);
-    }
-
-    //For Phrases Acitvity
-    public void openPhrasesActivity(View view) {
-        Intent intent = new Intent(this, PhrasesActivity.class);
-        startActivity(intent);
+        ButterKnife.bind(this);
+        // For Numbers Activity
+        numbers.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), NumbersActivity.class);
+            startActivity(intent);
+        });
+        //For Family Activity
+        family.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), FamilyActivity.class);
+            startActivity(intent);
+        });
+        //For Colors Activity
+        colors.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ColorsActivity.class);
+            startActivity(intent);
+        });
+        //For Phrases Activity
+        phrases.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), PhrasesActivity.class);
+            startActivity(intent);
+        });
     }
 }
