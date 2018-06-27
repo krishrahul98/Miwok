24cb5fef0462600bbeae31bb21b1b57cbba84dfb
Rahul Krishna <krishrahul98@gmail.com>
2018-06-28 00:54:07 +0530

    Fragment Adapter added and ViewPager in MainActivity

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,22 +1,10 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.numbers)
-    TextView numbers;
-    @BindView(R.id.family)
-    TextView family;
-    @BindView(R.id.colors)
-    TextView colors;
-    @BindView(R.id.phrases)
-    TextView phrases;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -25,26 +13,8 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
 
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        // For Numbers Activity
-        numbers.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), NumbersActivity.class);
-            startActivity(intent);
-        });
-        //For Family Activity
-        family.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), FamilyActivity.class);
-            startActivity(intent);
-        });
-        //For Colors Activity
-        colors.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), ColorsActivity.class);
-            startActivity(intent);
-        });
-        //For Phrases Activity
-        phrases.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), PhrasesActivity.class);
-            startActivity(intent);
-        });
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
     }
 }
