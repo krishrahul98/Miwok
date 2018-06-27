715e9b8e5e1fb1623a81423715f51fa3a374b18b
Rahul Krishna <krishrahul98@gmail.com>
2018-06-28 01:02:44 +0530

    TabLayout Added with ViewPager

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -13,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
 
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
+        //ButterKnife Bind View
+        ButterKnife.bind(this);
+        PageAdapter pageAdapter = new PageAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/example/android/miwok/PageAdapter.java
+++ app/src/main/java/com/example/android/miwok/PageAdapter.java
@@ -1,12 +1,17 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class PageAdapter extends FragmentPagerAdapter {
-    public PageAdapter(FragmentManager fm) {
+    private Context mContext;
+
+    public PageAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -19,6 +24,20 @@ public class PageAdapter extends FragmentPagerAdapter {
             return new ColorsFragment();
         } else {
             return new PhrasesFragment();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
         }
     }
 
